6ebe6a7dd45fe76a436970050631c017b01ba511
manhattan <kang_nace@hotmail.com>
2021-07-25 22:03:20 +0700

    update 3 flie main and nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
-import 'package:flutter/material.dart'; // มีคลาสควบคุมหน้าจอ ต้องเรียกใช้กรณีมีการใช้งานหน้าจอ
-import 'package:garagegood/screens/home.dart';
+import 'package:flutter/material.dart';
 
+import 'nav.dart';
 
-//void tpye จะมีชื่อเดียวกับคลาส จะเป็น constructo
-//stateless สร้างแล้ว ไม่สามารถแก้ไขได้
- void main() {
-    runApp(MyApp());
-  }
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Garage Good',
-      home: Home(),
+      title: 'Bottom navigation bar',
+      home: Nav(),
     );
   }
 }
